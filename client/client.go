065c19b85339bc98f2366b026febb3f96fc07216
night1008 <1179039379@qq.com>
2025-06-28 18:27:00 +0800

    add client

---
+++ client/client.go
@@ -0,0 +1,11 @@
+package client
+
+import (
+	"fmt"
+
+	"github.com/night1008/go-sdk-test/api"
+)
+
+func PrintFoo() {
+	fmt.Println(api.Foo)
+}
---
+++ client/go.mod
@@ -0,0 +1,5 @@
+module github.com/night1008/go-sdk-test/client
+
+go 1.23.5
+
+require github.com/night1008/go-sdk-test/api v0.1.0 // indirect
